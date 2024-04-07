a6eacf3a356012e8ad433da861a053026f74ac29
Odalis <odalislnunez@hotmail.com>
2024-04-07 13:06:47 -0400

    Adding IGeneridCrudService interface to add crud operations.

---
+++ ProductManagement.Core/Services/Interfaces/IGeneridCrudService.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManagement.Core.Services.Interfaces
+{
+    public interface IGeneridCrudService<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T> Get(int id);
+        public Task<bool> Create(T dto);
+        public Task<bool> Update(int id, T dto);
+        public Task<bool> Delete(int id);
+    }
+}

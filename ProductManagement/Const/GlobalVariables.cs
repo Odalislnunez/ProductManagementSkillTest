dcfa175820be51285c68b4887351f1d90a4d0142
Odalis <odalislnunez@hotmail.com>
2024-04-10 23:45:59 -0400

    Adding global variables.

---
+++ ProductManagement/Const/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManagement.Const
+{
+    public static class GlobalVariables
+    {
+        public const string Customer = "customers-list.rdlc";
+
+        public const string Item = "items-list.rdlc";
+
+        public const string CustomerItem = "customerItems-list.rdlc";
+    }
+}
